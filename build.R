0e9197588e68ee2c84b511fa43ff70cf0ab0ee12
yutannihilation <yutani.ini@gmail.com>
2015-08-22 19:13:57 +0900

    add build.R and create thumbnails

---
+++ build.R
@@ -0,0 +1,39 @@
+library(qiitr)
+library(rlist)
+library(pipeR)
+library(stringr)
+
+q <- Qiita$new(readLines(file("token"), n = 1))
+
+l <- q$get_item(tag_id = "ggplot2逆引き")
+
+create_thumbnail_dir <- function(item_id) {
+  
+}
+
+create_thumbnail <- function(item_id, images) {
+  thumbdir <- file.path("thumbnails", item_id)
+  dir.create(thumbdir, showWarnings = FALSE)
+  for (image in images) {
+    shell(sprintf('curl -k "%s" | convert - -resize 320x320 "%s/%s"', image, thumbdir, basename(image)))
+  }
+  
+  list.files(thumbdir, full.names = TRUE)
+}
+
+l %>>%
+  list.map(~ list(id = id,
+                  images = unlist(stringr::str_extract_all(.$body, 'https://qiita-image-store.*.(png|gif)')))) %>>%
+  str
+
+l[1] %>>%
+  list.map(~ list(url = jsonlite::unbox(.$url), title = jsonlite::unbox(.$title),
+                  images = create_thumbnail(
+                    item_id = .$id,
+                    images = unlist(
+                      str_extract_all(.$body, 'https://qiita-image-store.*.(png|gif)')
+                    )
+                  )
+           )) %>>%
+  jsonlite::toJSON(pretty = TRUE) %>>%
+  cat(file = "qiita.json")
